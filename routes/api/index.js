fbcbae067c28161830c2b051a885f12f0ac9b8ee
EunhaLim <dmsgk1559@gmail.com>
2022-09-29 16:52:11 +0900

    [Add] Get user info and user profile api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import auth from './auth.js';
+import user from './user.js';
 
 const router = Router();
 
 router.use('/auth', auth);
+router.use('/users', user);
 
 export default router;
---
+++ routes/api/user.js
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+import Sequelize from 'sequelize';
+import db from '../../models/index.js';
+const { User, Post, Media, MyPings, MyPingsPost, SharePings, sequelize } = db;
+const { QueryTypes } = sequelize;
+const { Op } = Sequelize;
+
+const router = Router();
+
+/* 사용자 정보 가져오기 */
+router.get('/:userId', (req, res) => {
+  User.findOne({
+    where: { id: req.params.userId },
+    attributes: ['id', 'nickname', 'profile_image_url']
+  })
+  .then(user => {
+    if (user === null) throw new Error('사용자를 찾을 수 없음')
+    res.status(200).json(user)
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({message: 'fail'});
+  });
+});
+
+/* 사용자 프로필 가져오기 */
+router.get('/:userId/profile', (req, res) => {
+  User.findOne({
+    where: { id: req.params.userId },
+    attributes: ['nickname', 'bio', 'profile_image_url']
+  })
+  .then(profile => {
+    if (profile === null) throw new Error('사용자를 찾을 수 없음');
+    res.json(profile);
+  })
+  .catch(err => {
+    console.error(err);
+    res.json({message: 'fail'});
+  });
+});
+
+export default router;
