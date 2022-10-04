bec868273ff76bdc50b35b5cb98dd4a7b98cc44a
minje <minje1409@gmail.com>
2022-10-04 21:00:42 +0900

    221004 2

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import auth from './auth.js';
+import profile from './profile.js';
 
 const router = Router();
 
 router.use('/auth', auth);
+router.use('/profile', profile);
 
 export default router;
