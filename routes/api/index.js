99d7b03e1e53c43db8ba606bd0c7588bbdf8cc2f
EunhaLim <dmsgk1559@gmail.com>
2022-10-04 16:10:08 +0900

    [Created] Mypings API / [Modified] routes/api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import auth from './auth.js';
 import user from './user.js';
+import mypings from './mypingsjs';
 
 const router = Router();
 
 router.use('/auth', auth);
 router.use('/users', user);
+router.use('/mypings', mypings);
 
 export default router;
