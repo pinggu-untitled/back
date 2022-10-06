2518d7ce92228443c1d784d71a2f97c72a9ecf2b
taeyun-work <wmfajr1357@gmail.com>
2022-10-07 02:48:46 +0900

    [Modified] result & searchHistory router refactoring

--- routes/data/index.js
+++ routes/data/index.js
@@ -2,5 +2,7 @@ import * as fileRepository from './file.js';
 import * as commentRepository from './comment.js';
 import * as likeRepository from './liked.js';
 import * as postRepository from './posts.js';
+import * as resultRepository from './result.js';
+import * as searchHistoryRepository from './searchHistories.js';
 
-export { fileRepository, commentRepository, likeRepository, postRepository };
+export { fileRepository, commentRepository, likeRepository, postRepository, resultRepository, searchHistoryRepository };
---
+++ routes/data/searchHistories.js
@@ -0,0 +1,43 @@
+import { USER_NUMBER } from '../controller/posts.js';
+
+export async function getAll(conn) {
+  console.log(USER_NUMBER);
+  return await conn
+    .execute(
+      `SELECT sh.id, sh.updated_at, s.content FROM SEARCHHISTORY as sh join SEARCH as s on sh.search = s.id WHERE sh.user = ? ORDER BY sh.updated_at desc;`,
+      [USER_NUMBER],
+    )
+    .then((res) => res[0]);
+}
+
+export async function create(conn, content) {
+  let searchId = await conn
+    .execute('INSERT ignore INTO SEARCH (content) values (?)', [content])
+    .then((res) => res[0].insertId);
+  if (!searchId) {
+    searchId = await conn.execute('SELECT id from SEARCH WHERE content = ?', [content]).then((res) => res[0][0].id);
+  }
+
+  try {
+    await conn.execute('INSERT INTO SEARCHHISTORY (user, search) values (?, ?)', [USER_NUMBER, searchId]);
+  } catch (err) {
+    await conn.execute('UPDATE SEARCHHISTORY SET updated_at = NOW() WHERE user = ? and search = ?', [
+      USER_NUMBER,
+      searchId,
+    ]);
+  }
+}
+
+export async function remove(conn) {
+  await conn.execute(`DELETE FROM SEARCHHISTORY as sh WHERE sh.user = ?`, [USER_NUMBER]);
+}
+export async function removeById(conn, historyId) {
+  await conn.execute(`DELETE FROM SEARCHHISTORY as sh WHERE sh.id = ? and sh.user = ?`, [historyId, USER_NUMBER]);
+}
+export async function getPopular(conn) {
+  return await conn
+    .execute(
+      `SELECT s.content, count(*) as count FROM SEARCHHISTORY as sh join SEARCH as s on sh.search = s.id group by s.content order by count desc limit 10`,
+    )
+    .then((res) => res[0]);
+}
