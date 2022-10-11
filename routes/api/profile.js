878d9773adafc1df4fc3ef263089655cdbba18b9
minje <minje1409@gmail.com>
2022-10-11 22:36:48 +0900

    profile 수정

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,13 @@
-// import express, { json } from 'express';
-// import {} from 'express-async-errors';
+import express from 'express';
 import { profileController } from '../controller/index.js';
-// import { upload } from '../middlewares/upload.js';
+import { upload } from '../middlewares/upload.js';
 const profileRouter = express.Router();
 
-profileRouter.post('/image', profileController.updateProfileImage);
+profileRouter.post('/image', upload.single('file'), profileController.updateProfileImage);
+// profileRouter.post('/image', profileController.updateProfileImage, (req, res, next) => {
+//   console.log(11);
+//   return res.status(201).json(req.file.name);
+// })
 profileRouter.patch('/info', profileController.updateProfileInfo);
+
+export default profileRouter;
--- routes/controller/profile.js
+++ routes/controller/profile.js
@@ -1,24 +1,76 @@
 import { db } from '../../config/mysql.js';
 import { profileRepository } from '../data/index.js';
+import {unlink} from 'fs';
+import { resolve } from 'path';
+
+const deleteFile = file => new Promise(resolve, reject => {
+  // console.log('deleteFile() file :>> ', file);
+  unlink(file, err => {
+    if (err) reject(err);
+  })
+  resolve(true);
+})
 
 export async function updateProfileInfo(req, res, next) {
-  const NEW_USER = {
-    id: req.user.id,
-    nickname: req.param.nickname,
-    bio: req.param.bio,
+  // test 값 박아놓은 것.
+  const TMPUSER = {
+    id: 17,
+    nickname: '2438690663',
+    profile_image_url: 'http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_110x110.jpg',
   }
 
-  // req.session.passport.user; // 세션에 user객체
+  req.user = TMPUSER;
+  req.user.id = 17;
+  req.session.passport = {};
+  req.session.passport.user = {
+    id: 17,
+    nickname: '2438690663',
+    profile_image_url: 'http://k.kakaocdn.net/dn/dpk9l1/btqmGhA2lKL/Oz0wDuJn1YV2DIn92f6DVK/img_110x110.jpg',
+  }
+  // test 값 끝
+
+  const NEW_USER = {
+    id: req.user.id,
+    nickname: req.body.nickname,
+    bio: req.body.bio,
+    profile_image_url: req.body.profile_image_url,
+  }
+
+  // console.log('NEW_USER :>> ', NEW_USER);
+  // console.log('req.session.passport.user.profile_image_url :>> ', req.session.passport.user.profile_image_url);
+
+  const isImage = req.session.passport.user.profile_image_url.indexOf('/https|http/') < 0; // 기존 프로필 사진이 이미지일 경우 true
+  // return res.status(201).json();
+
   const conn = await db.getConnection();
   try {
-    // bio에 helmet으로 보안 체크
 
-    console.log('update 시작');
-    const res = await profileRepository.updateProfileInfo(conn, NEW_USER);
-    console.log('update 성공');
+    const result = await profileRepository.updateProfileInfo(conn, NEW_USER);
+
+    // session 정보 변경
     req.session.passport.user.nickname = NEW_USER.nickname;
+    req.session.passport.user.profile_image_url = NEW_USER.profile_image_url;
 
-  } catch (e) {
+    // console.log('req.session.passport.user.nickname :>> ', req.session.passport.user.nickname);
+    // console.log('req.session.passport.user.profile_image_url :>> ', req.session.passport.user.profile_image_url);
+
+    // console.log('isImage11 :>> ', isImage);
+    // console.log('fs.existsSync(NEW_USER.profile_image_url)11 :>> ', fs.existsSync(NEW_USER.profile_image_url));
+    if (isImage && fs.existsSync(NEW_USER.profile_image_url)) {
+      // console.log('isImage22 :>> ', isImage);
+      // console.log('fs.existsSync(NEW_USER.profile_image_url)22 :>> ', fs.existsSync(NEW_USER.profile_image_url));
+
+      // promise로 바꾸기 221006
+      const deleteRes = await deleteFile(NEW_USER.profile_image_url);
+      if(!deleteRes) {
+        // console.log('failed file name : ', NEW_USER.profile_image_url);
+        throw new Error('delete file fail!!!');
+      }
+    }
+
+    return res.status(200).json();
+
+  } catch (err) {
     return res.status(500).json(err);
   } finally {
     conn.release();
@@ -26,18 +78,6 @@ export async function updateProfileInfo(req, res, next) {
 }
 
 export async function updateProfileImage(req, res, next) {
-  const isImage = req.session.passport.user.profile_image_url.indexOf('/https|http/') < 0; // 프로토콜이 http https 일 경우 false
-
-  // 기존 이미지가 url일 경우, 파일 생성 후 DB 업데이트
-    
-  // 기존 이미지가 Image 일 경우, 기존 파일 삭제 / 생성 후 DB 업데이트
-
-  const conn = await db.getConnection();
-  try {
-    
-  } catch (e) {
-    return res.status(500).json(err);
-  } finally {
-    conn.release();
-  }
+  // console.log('req.file.filename', req.file.filename);
+  return res.status(200).json(req.file.name);
 }
