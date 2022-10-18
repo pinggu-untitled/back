a4c3c10ddb61cc239f54bafcd038b2c2f59a2605
EunhaLim <dmsgk1559@gmail.com>
2022-10-18 18:18:42 +0900

    [Modified] (routes/api/followingPosts.js) async/await -> promise

--- routes/api/followingPosts.js
+++ routes/api/followingPosts.js
@@ -5,40 +5,20 @@ const { Follow, Post, Media } = db;
 
 const router = Router();
 
-// router.get('/posts', async (req, res) => {
-//   try {
-//     /* 현재 로그인한 사용자가 팔로우하고 있는 친구의 아이디 값 가져오기 */
-//     const followings = await Follow.findAll({
-//       where: {
-//         host: req.user.id
-//       },
-//       attributes: ['follow']
-//     });
-//     const followingIdArray = followings.map(following => following.follow);
-
-//     /* 가져온 팔로잉 친구들의 포스트 가져오기 - join Media*/
-//     const posts = await Post.findAll({
-//       include: [{
-//         model: Media,
-//         attributes: ['src'],
-//         limit: 1
-//       }],
-//       where: {
-//         user: {
-//           [Op.in]: followingIdArray
-//         }
-//       },
-//       attributes: ['id', 'created_at', 'title', 'hits', 'is_private']
-//     });
-//     res.status(200).json(posts);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: 'fail' });
-//   }
-// });
 router.get('/posts', (req, res) => {
+  /* 현재 로그인한 사용자가 팔로우하고 있는 사용자 아이디 가져오기 */
   Follow.findAll({ where: { host: req.user.id }, attributes: ['follow']})
-    .then(followings => followings.map(following => following.follow));
+    .then(followings => followings.map(following => following.follow))
+    .then(followingIdArray => 
+      /* 팔로잉중인 사용자의 포스트 가져오기 */
+      Post.findAll({
+        include: [{ model: Media, attributes: ['src'], limit: 1}],
+        where: { user: { [Op.in]: followingIdArray } },
+        attributes: ['id', 'created_at', 'title', 'hits', 'is_private']
+      })
+    )
+    .then(posts => res.status(200).json(posts))
+    .catch(err => res.status(500).json({ message: 'fail' }))
 });
 
 export default router;
