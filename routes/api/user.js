123a55b5088b391a1ba6a9872e4b3fc0f236ebbc
EunhaLim <dmsgk1559@gmail.com>
2022-10-19 01:51:47 +0900

    [Remove] Get following's posts API. [Modified] (routes/api/mypings.js) async/await -> promise

--- routes/api/user.js
+++ routes/api/user.js
@@ -11,16 +11,14 @@ const router = Router();
 router.get('/me', (req, res) => {
   if (req.user?.id) {
     res.status(200).json(req.session.passport.user);
-  }
-  else
-    res.status(500).json(null);
+  } else res.status(500).json(null);
 });
 
 /* 모든 사용자 정보 가져오기 */
 router.get('/all', (req, res) => {
-  User.findAll({ attributes: ['id', 'nickname', 'profile_image_url']})
-    .then(users => res.status(200).json(users))
-    .catch(err => {
+  User.findAll({ attributes: ['id', 'nickname', 'profile_image_url'] })
+    .then((users) => res.status(200).json(users))
+    .catch((err) => {
       console.error(err);
       res.status(500).json({ message: 'fail' });
     });
@@ -30,56 +28,58 @@ router.get('/all', (req, res) => {
 router.get('/:userId', (req, res) => {
   User.findOne({
     where: { id: req.params.userId },
-    attributes: ['id', 'nickname', 'bio', 'profile_image_url']
+    attributes: ['id', 'nickname', 'bio', 'profile_image_url'],
   })
-  .then(user => {
-    if (user === null) throw new Error('사용자를 찾을 수 없음')
-    res.status(200).json(user)
-  })
-  .catch(err => {
-    console.error(err);
-    res.status(500).json({message: 'fail'});
-  });
+    .then((user) => {
+      if (user === null) throw new Error('사용자를 찾을 수 없음');
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: 'fail' });
+    });
 });
-
 
 /* 팔로워 목록 가져오기 */
 router.get('/:userId/followers', (req, res) => {
-  const query = 'SELECT USER.id, USER.nickname, USER.profile_image_url FROM USER INNER JOIN FOLLOW ON (FOLLOW.host=USER.id) WHERE FOLLOW.follow=:userId';
-  sequelize.query(query, {
-    type: QueryTypes.SELECT,
-    replacements: { userId: req.params.userId }
-  })
-    .then(followers => res.status(200).json(followers))
-    .catch(err => res.status(500).json({ message: 'fail' }));
+  const query =
+    'SELECT USER.id, USER.nickname, USER.profile_image_url FROM USER INNER JOIN FOLLOW ON (FOLLOW.host=USER.id) WHERE FOLLOW.follow=:userId';
+  sequelize
+    .query(query, {
+      type: QueryTypes.SELECT,
+      replacements: { userId: req.params.userId },
+    })
+    .then((followers) => res.status(200).json(followers))
+    .catch((err) => res.status(500).json({ message: 'fail' }));
 });
 
 /* 팔로잉 목록 가져오기 */
 router.get('/:userId/followings', (req, res) => {
-  const query = 'SELECT USER.id, USER.nickname, USER.profile_image_url FROM USER INNER JOIN FOLLOW ON (FOLLOW.follow=USER.id) WHERE FOLLOW.host=?';
-  sequelize.query(query, {
-    type: QueryTypes.SELECT,
-    replacements: [req.params.userId]
-  })
-    .then(followings => res.status(200).json(followings))
-    .catch(err => res.status(500).json({ message: 'fail' }));
+  const query =
+    'SELECT USER.id, USER.nickname, USER.profile_image_url FROM USER INNER JOIN FOLLOW ON (FOLLOW.follow=USER.id) WHERE FOLLOW.host=?';
+  sequelize
+    .query(query, {
+      type: QueryTypes.SELECT,
+      replacements: [req.params.userId],
+    })
+    .then((followings) => res.status(200).json(followings))
+    .catch((err) => res.status(500).json({ message: 'fail' }));
 });
-
 
 /* 사용자의 마이핑스 전체 목록 가져오기 */
 router.get('/:userId/mypings', (req, res) => {
   MyPings.findAll({
     where: {
-      user: req.params.userId
+      user: req.params.userId,
     },
-    attributes: ['id', 'title', 'category', 'user', 'is_private']
+    attributes: ['id', 'title', 'category', 'user', 'is_private'],
   })
-  .then(mypings => {
-    res.status(200).json(mypings);
-  })
-  .catch(err => {
-    res.status(500).json({message: 'fail'});
-  });
+    .then((mypings) => {
+      res.status(200).json(mypings);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'fail' });
+    });
 });
 
 /* 사용의 특정 마이핑스 가져오기 */
@@ -87,45 +87,50 @@ router.get('/:userId/mypings/:mypingsId', (req, res) => {
   MyPings.findOne({
     where: {
       id: req.params.mypingsId,
-      user: req.params.userId
+      user: req.params.userId,
     },
-    attributes: ['id', 'title', 'category', 'user', 'is_private']
+    attributes: ['id', 'title', 'category', 'user', 'is_private'],
   })
-  .then(mypings => {
-    res.status(200).json(mypings);
-  })
-  .catch(err => {
-    res.status(500).json({message: 'fail'});
-  });
+    .then((mypings) => {
+      res.status(200).json(mypings);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: 'fail' });
+    });
 });
 
 /* 공유된 마이핑스 가져오기 */
 router.get('/:userId/sharepings', (req, res) => {
   SharePings.findAll({ where: { guest: req.params.userId }, attributes: ['mypings'] })
-    .then(sharedArray => sharedArray.map(sharedObj => sharedObj.mypings))
-    .then(sharedIdArray => 
-      MyPings.findAll({ where: { id: { [Op.in]: sharedIdArray }}, attributes: ['id', 'title', 'category', 'user', 'is_private']})
+    .then((sharedArray) => sharedArray.map((sharedObj) => sharedObj.mypings))
+    .then((sharedIdArray) =>
+      MyPings.findAll({
+        where: { id: { [Op.in]: sharedIdArray } },
+        attributes: ['id', 'title', 'category', 'user', 'is_private'],
+      }),
     )
-    .then(result => res.status(200).json(result))
-    .catch(err => res.status(500).json({ message: 'fail' }));
+    .then((result) => res.status(200).json(result))
+    .catch((err) => res.status(500).json({ message: 'fail' }));
 });
 
 /* 내 혹은 유저의 포스트 전체 목록 가져오기  */
 router.get('/:userId/posts', (req, res) => {
   Post.findAll({
-    include: [{
-      model: Media,
-      attributes: ['src'],
-      limit: 1
-    }],
+    include: [
+      {
+        model: Media,
+        attributes: ['src'],
+        limit: 1,
+      },
+    ],
     attributes: ['id', 'created_at', 'title', 'hits', 'is_private'],
-    where: { user: req.params.userId }
+    where: { user: req.params.userId },
   })
-  .then(posts => res.status(200).json(posts))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({message: 'fail'});
-  });
+    .then((posts) => res.status(200).json(posts))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: 'fail' });
+    });
 });
 
 /* 특정 마이핑스에 속한 포스트 목록 가져오기 */
@@ -135,17 +140,17 @@ router.get('/:userId/posts', (req, res) => {
  * WHERE mp.mypings=:mypingsId
  */
 router.get('/:userId/mypings/:mypingsId/posts', (req, res) => {
-  MyPingsPost.findAll({ where: { mypings: req.params.mypingsId }, attributes: ['post']})
-    .then(postArray => postArray.map(postObj => postObj.post))
-    .then(postIdArray => 
+  MyPingsPost.findAll({ where: { mypings: req.params.mypingsId }, attributes: ['post'] })
+    .then((postArray) => postArray.map((postObj) => postObj.post))
+    .then((postIdArray) =>
       Post.findAll({
-        include: [{ model: Media, attributes: ['src'], limit: 1}],
+        include: [{ model: Media, attributes: ['src'], limit: 1 }],
         where: { id: { [Op.in]: postIdArray } },
-        attributes: ['id', 'created_at', 'title', 'hits', 'is_private']
-      })
+        attributes: ['id', 'created_at', 'title', 'hits', 'is_private'],
+      }),
     )
-    .then(posts => res.status(200).json(posts))
-    .catch(err => {
+    .then((posts) => res.status(200).json(posts))
+    .catch((err) => {
       console.error(err);
       res.status(500).json({ message: 'fail' });
     });
