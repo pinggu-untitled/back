ba9d9d45e380f7aa6df8dbbbcdb089ac7835fafa
EunhaLim <dmsgk1559@gmail.com>
2022-10-19 16:16:21 +0900

    [Before Modified] follow api

--- routes/api/follow.js
+++ routes/api/follow.js
@@ -8,25 +8,27 @@ const router = express.Router();
 router.post('/:userId', (req, res) => {
   console.log(req.user);
   Follow.create({ host: req.user.id, follow: req.params.userId })
-  .then(result => { // 성공 시 insert 된 로우 반환
-    res.status(200).json({message: 'success'});
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({message: 'fail'});
-  });
+    .then((result) => {
+      // 성공 시 insert 된 로우 반환
+      res.status(200).json({ message: 'success' });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: 'fail' });
+    });
 });
 
 /* 언팔로우하기 */
 router.delete('/:userId', (req, res) => {
-  Follow.destroy({ where: { host: req.user.id, follow: req.params.userId }})
-  .then(result => { // 성공 시 delete 된 로우 수 반환
-    res.status(200).json({message: 'success'});
-  })
-  .catch(err => {
-    console.log( err);
-    res.status(500).json({message: 'fail'});
-  });
+  Follow.destroy({ where: { host: req.user.id, follow: req.params.userId } })
+    .then((result) => {
+      // 성공 시 delete 된 로우 수 반환
+      res.status(200).json({ message: 'success' });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ message: 'fail' });
+    });
 });
 
 export default router;
