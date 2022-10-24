5a521fa41e1b12c9d08a437010097fcdf1d02818
taeyun-work <wmfajr1357@gmail.com>
2022-10-24 22:04:48 +0900

    [Modified]

--- routes/middlewares/accessible.js
+++ routes/middlewares/accessible.js
@@ -4,12 +4,12 @@ const { MyPings, Post, Comment } = db;
 export const isAccessible = async (req, res, next) => {
   try {
     let result;
-    if (req.originalUrl.includes('posts')) {
+    if (req.originalUrl.includes('comments')) {
+      result = await Comment.findOne({ where: { id: req.params.commentsId }, attributes: ['user'] });
+    } else if (req.originalUrl.includes('posts')) {
       result = await Post.findOne({ where: { id: req.params.postId }, attributes: ['user'] });
     } else if (req.originalUrl.includes('mypings')) {
       result = await MyPings.findOne({ where: { id: req.params.mypingsId }, attributes: ['user'] });
-    } else if (req.originalUrl.includes('comments')) {
-      result = await Comment.findOne({ where: { id: req.params.commentsId }, attributes: ['user'] });
     }
     if (result?.user !== req.user.id) throw new Error('접근 권한이 없습니다.');
     next();
