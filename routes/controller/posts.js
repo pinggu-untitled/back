e78f1cd277646661c0653b5245e0c967dfacedf3
taeyun-work <wmfajr1357@gmail.com>
2022-11-02 21:31:25 +0900

    [Add] Explore tab map api

--- routes/controller/posts.js
+++ routes/controller/posts.js
@@ -273,14 +273,14 @@ export async function removePost(req, res, next) {
 }
 
 export async function getByBounds(req, res, next) {
-  const { swLat, swLng, neLat, neLng, tab } = req.query;
+  const { swLat, swLng, neLat, neLng, tab, filter, keyword } = req.query;
   const conn = await db.getConnection();
   const userId = req.user.id;
   try {
     let result;
     switch (tab) {
       case 'home':
-        result = await postRepository.getByBounds(conn, userId, swLat, neLat, swLng, neLng);
+        result = await postRepository.getByBoundsInHome(conn, userId, swLat, neLat, swLng, neLng);
         // result = await Promise.all(
         //   result.map(async (post) => {
         //     post.Images = await fileRepository.getAll(conn, post.id);
@@ -319,6 +319,32 @@ export async function getByBounds(req, res, next) {
 
       //TODO 탐색탭
       case 'explore':
+        result = await postRepository.getByBoundsInExplore(conn, swLat, neLat, swLng, neLng, filter, keyword);
+        result = await Promise.all(
+          result.map(async (post) => {
+            post.Images = await fileRepository.getAll(conn, post.id);
+            if (post.Images.length !== 0) {
+              post.Images = post.Images[0];
+            }
+            return {
+              id: post.id,
+              title: post.title,
+              content: post.content,
+              longitude: post.longitude,
+              latitude: post.latitude,
+              hits: post.hits,
+              is_private: post.is_private,
+              created_at: post.created_at,
+              updated_at: post.updated_at,
+              User: {
+                id: post.userId,
+                nickname: post.nickname,
+                profile_image_url: post.profile_image_url,
+              },
+              Images: post.Images,
+            };
+          }),
+        );
         return res.status(200).json(result);
       default:
         return res.status(403).json({ message: 'invalid tab' });
